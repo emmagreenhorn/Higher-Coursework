9f224b0ab93c1f8923c78931e07406f7d6fc9c9c
emmagreenhorn <Guest@stk-cs01-ws11.westlothian.sch.uk>
2015-12-15 02:19:15 -0800

    sprint 1

---
+++ CUSTOMER.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class CUSTOMER here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CUSTOMER
+{
+   
+    public CUSTOMER()
+    {
+        // initialise instance variables
+       private string CustomerID;
+       private char TicketID;
+       private char method;
+       private int NumofTickets
+       
+       
+    }
+
+   public CUSTOMER()
+   {
+       CustomerID ="";
+       TicketID =
+       
+    }
+}
